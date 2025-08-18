6867096bbf36559e3e7b96481cbb7268db3e2cbb
Tanmay2912 <149605165+Tanmay2912@users.noreply.github.com>
2025-08-18 22:40:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (83.68%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int findMaxArray(int[] nums) {
+        int max = nums[0];
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        return max;
+    }
+    public int findSumArray(int[] nums) {
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+    public int findCountPart(int[] nums, int mid) {
+        int count = 1, sumArr = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(sumArr + nums[i] <= mid) {
+                sumArr += nums[i];
+            }
+            else {
+                count ++;
+                sumArr = nums[i];
+            }
+        }
+        return count;  
+    }
+    public int splitArray(int[] nums, int k) {
+        int low = findMaxArray(nums);
+        int high = findSumArray(nums);
+        while(low <= high) {
+            int mid = (low + high)/2;
+            int countPart = findCountPart(nums, mid);
+            if(countPart > k) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
