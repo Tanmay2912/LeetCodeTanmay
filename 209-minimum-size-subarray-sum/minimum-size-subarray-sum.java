8dde28d855aa8657af314eeb15b5043e3b9df2a0
Tanmay2912 <149605165+Tanmay2912@users.noreply.github.com>
2025-08-19 18:51:15 +0530

    Time: 13 ms (3.80%) | Memory: 58.4 MB (30.37%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,32 +1,37 @@
 class Solution {
-    public int minSubArrayLen(int target, int[] nums) {
-        int n = nums.length;
-        int[] prefix = new int[n + 1]; 
-        for (int i = 1; i <= n; i++) {
-            prefix[i] = prefix[i - 1] + nums[i - 1];
-        }
-        int minLen = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            int required = target + prefix[i];
-            int j = binarySearch(prefix, required); 
-            if (j != -1) {
-                minLen = Math.min(minLen, j - i);
-            }
-        }
-        return minLen == Integer.MAX_VALUE ? 0 : minLen;
-    }
-    private int binarySearch(int[] prefix, int target) {
-        int left = 0, right = prefix.length - 1;
+    public int binarySearch(int[] prefix, int required) {
+        int low = 0, high = prefix.length-1;
         int ans = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (prefix[mid] >= target) {
+        while(low <= high) {
+            int mid = (low + high)/2;
+            if(prefix[mid] >= required) {
                 ans = mid;
-                right = mid - 1; 
-            } else {
-                left = mid + 1;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
             }
         }
         return ans;
+    }
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        if(n == 0) { return 0;}
+        int prefix[] = new int[n+1];
+        for(int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i-1] + nums[i-1];
+        }
+        int minLen = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++) {
+            int required = target + prefix[i];
+            int j = binarySearch(prefix, required);
+            if(j != -1) {
+                minLen = Math.min(minLen, j-i);
+            }
+        }
+        if(minLen == Integer.MAX_VALUE) {
+            return 0;
+        }
+        return minLen;
     }
 }
