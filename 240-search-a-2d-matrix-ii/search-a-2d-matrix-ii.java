bcb31dc30bc2573d85a4ef2398f9614f72874027
Tanmay2912 <149605165+Tanmay2912@users.noreply.github.com>
2025-08-19 22:50:00 +0530

    Time: 5 ms (99.18%) | Memory: 46 MB (59.68%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,27 +1,39 @@
 class Solution {
-    public boolean findInRow(int[] row, int target) {
-        int low = 0, high = row.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (row[mid] == target) {
-                return true;
-            } else if (row[mid] > target) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return false;
-    }
+    // public boolean findInRow(int[] row, int target) {
+    //     int low = 0, high = row.length - 1;
+    //     while (low <= high) {
+    //         int mid = (low + high) / 2;
+    //         if (row[mid] == target) {
+    //             return true;
+    //         } else if (row[mid] > target) {
+    //             high = mid - 1;
+    //         } else {
+    //             low = mid + 1;
+    //         }
+    //     }
+    //     return false;
+    // }
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-
-        for (int i = 0; i < rows; i++) {
-            if (target >= matrix[i][0] && target <= matrix[i][cols - 1]) {
-                if (findInRow(matrix[i], target)) {
-                    return true;
-                }
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int row = 0;
+        int col = m - 1;
+        // for (int i = 0; i < rows; i++) {
+        //     if (target >= matrix[i][0] && target <= matrix[i][cols - 1]) {
+        //         if (findInRow(matrix[i], target)) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        while(row < n && col >= 0) {
+            if(matrix[row][col] == target) {
+                return true;
+            }
+            else if(matrix[row][col] < target) {
+                row++;
+            }
+            else {
+                col--;
             }
         }
         return false;
