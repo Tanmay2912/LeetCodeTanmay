bc22ee37097c622528246d0fd27d3e74d151abd9
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2025-09-03 00:30:34 +0530

    Time: 15 ms (92.49%) | Memory: 48.1 MB (79.97%) - LeetSync

--- 817-design-hashmap/design-hashmap.java
+++ 817-design-hashmap/design-hashmap.java
@@ -1,76 +1,73 @@
 class ListNode {
-    int key;
-    int value;
+    int key, value;
     ListNode next;
+
     ListNode(int key, int value) {
         this.key = key;
         this.value = value;
-        this.next = null;
     }
 }
 
 class MyHashMap {
-
     private ListNode[] map;
 
     public MyHashMap() {
-        map = new ListNode[1000];
-        for (int i = 0; i < 1000; i++) {
-            map[i] = new ListNode(-1, -1); 
-        }
+        map = new ListNode[1000]; 
     }
-    
+
+    private int hash(int key) {
+        return key % 1000; 
+    }
+
     public void put(int key, int value) {
+        int hash = hash(key);
+
+        if (map[hash] == null) {
+            map[hash] = new ListNode(key, value);
+            return;
+        }
+
+        ListNode curr = map[hash];
+        while (true) {
+            if (curr.key == key) {
+                curr.value = value;
+                return;
+            }
+            if (curr.next == null) break; 
+            curr = curr.next;
+        }
+
+        curr.next = new ListNode(key, value);
+    }
+
+    public int get(int key) {
         int hash = hash(key);
         ListNode curr = map[hash];
 
-        while (curr.next != null) {
-            if (curr.next.key == key) {
-                curr.next.value = value; 
-                return;
-            }
-            curr = curr.next;
-        }
-
-        curr.next = new ListNode(key, value); 
-    }
-    
-    public int get(int key) {
-        int hash = hash(key);
-        ListNode curr = map[hash].next;
-
         while (curr != null) {
-            if (curr.key == key) {
-                return curr.value;
-            }
+            if (curr.key == key) return curr.value;
             curr = curr.next;
         }
-
-        return -1;
+        return -1; 
     }
-    
+
     public void remove(int key) {
         int hash = hash(key);
         ListNode curr = map[hash];
 
+        if (curr == null) return;
+
+        if (curr.key == key) {
+            map[hash] = curr.next;
+            return;
+        }
+
         while (curr.next != null) {
             if (curr.next.key == key) {
-                curr.next = curr.next.next; 
-                return; 
+                curr.next = curr.next.next;
+                return;
             }
             curr = curr.next;
         }
     }
-
-    private int hash(int key) {
-        return key % 1000;
-    }
 }
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
- */
