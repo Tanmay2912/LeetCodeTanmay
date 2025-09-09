fa3b798111338630022df09eeb7277b59e1aecd9
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2025-09-09 22:53:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (50.11%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,25 +1,41 @@
+// class Solution {
+//     public int strStr(String haystack, String needle) {
+//         /*int i = 0, j = 0;
+//         int index = -1;
+//         while(i < haystack.length() && j < needle.length()) {
+//             if(haystack.charAt(i) == needle.charAt(j)) {
+//                 i++; j++;
+//                 continue;
+//             }
+//             else {
+//                 i++;
+//             }
+//         }
+//         if(needle == null) return -1; 
+//         if(haystack.isEmpty()) return 0; 
+//         if(needle.length() > haystack.length()) return -1;
+//         for(int i = 0; i < haystack.length()-needle.length(); i++) {
+//             if(haystack.substring(i, i+needle.length()).equals(needle)) {
+//                 return i;
+//             }
+//         }
+//         return -1;*/
+//         return haystack.indexOf(needle);
+//     }
+// }
+
 class Solution {
     public int strStr(String haystack, String needle) {
-        /*int i = 0, j = 0;
-        int index = -1;
-        while(i < haystack.length() && j < needle.length()) {
-            if(haystack.charAt(i) == needle.charAt(j)) {
-                i++; j++;
-                continue;
-            }
-            else {
-                i++;
-            }
-        }
-        if(needle == null) return -1; 
-        if(haystack.isEmpty()) return 0; 
-        if(needle.length() > haystack.length()) return -1;
-        for(int i = 0; i < haystack.length()-needle.length(); i++) {
-            if(haystack.substring(i, i+needle.length()).equals(needle)) {
-                return i;
+        if (needle.length() == 0) return 0;   // empty needle -> return 0
+        int h = haystack.length();
+        int n = needle.length();
+
+        // only iterate while there is enough room for the needle
+        for (int i = 0; i <= h - n; i++) {
+            if (haystack.substring(i, i + n).equals(needle)) {
+                return i; // first occurrence found
             }
         }
-        return -1;*/
-        return haystack.indexOf(needle);
+        return -1; // not found
     }
 }
