d10973d055e1b832d6c0ec7d2b9d88673ae2691c
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2025-10-05 23:37:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (74.35%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.java
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -2,11 +2,12 @@ class Solution {
     public int[] decrypt(int[] code, int k) {
         int n = code.length;
         int ans[] = new int[n];
+        // agar k 0 hai tho 0 ka hi array return kar do
         if(k == 0) {
             Arrays.fill(ans, 0);
             return ans;
         }
-        
+        // ek circular array bana lo simple of size 2n aur i+n will be i only
         int circular[] = new int[2*n];
         for(int i = 0; i < n; i++) {
             circular[i] = code[i];
@@ -14,30 +15,31 @@ class Solution {
         }
         
         if(k > 0) {
-            int sum = 0, i = 0, j = 1; // start j from i+1 for "next k elements"
-            // initialize first window
+            // j ko 1 liya hai kyuki uss element ko include nhi karna hai sum meh next element 
+            int sum = 0, i = 0, j = 1; 
+            // initialize first window pahla sum nikal lo
             while(j <= k) {
                 sum += circular[j];
                 j++;
             }
-            
+            // first window hi hai tho i = 0 ab j 2n tak jayega lekin i n tak hi rahega kyuki utna hi chahiye hame
             i = 0;
             while(i < n) {
                 ans[i] = sum;
-                // slide window: remove first element of window, add next element
+                // remove first element of window, add next element
                 sum = sum - circular[i + 1] + circular[i + k + 1];
                 i++;
             }
         }
         else {
             k = -k;
+            // is baar j ko n-k kiya hai kyuki previous elements ka sum chahaiye
             int sum = 0, i = 0, j = n - k;
             // initialize first window of previous k elements
             while(j < n) {
                 sum += circular[j];
                 j++;
             }
-            
             i = 0;
             j = n;
             while(i < n) {
