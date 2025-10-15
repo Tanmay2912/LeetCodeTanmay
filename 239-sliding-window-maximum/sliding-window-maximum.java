eda2435b9ea3226ef85633bae518841870fc0ed7
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2025-10-16 00:36:17 +0530

    Time: 31 ms (64.05%) | Memory: 62.3 MB (20.76%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,36 +1,29 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        if (n == 0 || k == 0) return new int[0];
-
-        int[] left = new int[n];
-        int[] right = new int[n];
-
-        left[0] = nums[0];
-        right[n - 1] = nums[n - 1];
-
-        for (int i = 1; i < n; i++) {
-            
-            if (i % k == 0) {
-                left[i] = nums[i]; 
-            } else {
-                left[i] = Math.max(left[i - 1], nums[i]);
+        int i = 0, j = 0;
+        Deque<Integer> dq = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+        while(j < nums.length) {
+            while(!dq.isEmpty() && dq.peekLast() < nums[j]) {
+                dq.pollLast();
             }
-
-            
-            int j = n - i - 1;
-            if ((j + 1) % k == 0) {
-                right[j] = nums[j]; 
-            } else {
-                right[j] = Math.max(right[j + 1], nums[j]);
+            dq.offerLast(nums[j]);
+            if(j-i+1 < k) {
+                j++;
+            }
+            else if(j-i+1 == k) {
+                ans.add(dq.peekFirst());
+                if(dq.peekFirst() == nums[i]) {
+                    dq.pollFirst();
+                }
+                i++;
+                j++;
             }
         }
-
-        int[] result = new int[n - k + 1];
-        for (int i = 0; i <= n - k; i++) {
-            result[i] = Math.max(right[i], left[i + k - 1]);
+        int[] res = new int[ans.size()];
+        for (int x = 0; x < ans.size(); x++) {
+            res[x] = ans.get(x);
         }
-
-        return result;
+        return res;
     }
 }
