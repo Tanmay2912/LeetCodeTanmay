0b68a4f4fa97dc857217af40cabf40ed29e398cd
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2025-11-14 15:54:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (15.94%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+        int rows = obstacleGrid.length;
+        int cols = obstacleGrid[0].length;
+        int[] dp = new int[cols];
+        dp[0] = 1;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                } else {
+                    if (j > 0) {
+                        dp[j] += dp[j - 1];
+                    }
+                }
+            }
+        }
+        return dp[cols - 1];        
+    }
+}
