19d5e87483b6ad8ad65cd85e6bb443f1beec8335
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2025-11-17 12:35:04 +0530

    Time: 1 ms (99.71%) | Memory: 65.6 MB (9.43%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
@@ -1,35 +1,12 @@
-// class Solution {
-//     public boolean kLengthApart(int[] nums, int k) {
-//         int prev = -1;  
-//         for (int i = 0; i < nums.length; i++) {
-//             if (nums[i] == 1) {
-//                 if (prev != -1 && (i - prev - 1) < k) {
-//                     return false;  
-//                 }
-//                 prev = i; 
-//             }
-//         }
-//         return true;
-//     }
-// }
 class Solution {
-    static
-    {
-        for(int i=0;i<100;i++)kLengthApart(new int[]{0}, 2);
-    }
-    public static boolean kLengthApart(int[] nums, int k) {
-        int c=k,m=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==1)
-            {
-                if(c<k)return false;
-                m=1;
-                c=0;
-            }
-            else
-            {
-                if(m==1)c++;
+    public boolean kLengthApart(int[] nums, int k) {
+        int prev = -1;  
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                if (prev != -1 && (i - prev - 1) < k) {
+                    return false;  
+                }
+                prev = i; 
             }
         }
         return true;
