574a6413f3d386a18cb7fdb1cbf877f397592bc3
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2025-12-17 19:53:25 +0530

    Time: 1 ms (11.96%) | Memory: 43.3 MB (17.39%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -14,17 +14,35 @@
  * }
  */
 class Solution {
-    public List<Integer> traversal(TreeNode root, List<Integer> ans) {
-        if(root == null) {
-            return ans;
-        }
+    // this is recursive method
+    public void Traversal(TreeNode root, List<Integer> ans) {
+        if(root == null) 
+            return;
         traversal(root.left, ans);
         traversal(root.right, ans);
         ans.add(root.val);
-        return ans;
+    }
+    // this is iterative method
+    public void traversal(TreeNode root, List<Integer> ans) {
+        if(root == null) return;
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        st1.push(root);
+        while(!st1.isEmpty()) {
+            root = st1.pop();
+            st2.push(root);
+            if(root.left != null) 
+                st1.push(root.left);
+            if(root.right != null)
+                st1.push(root.right);
+        }
+        while(!st2.isEmpty()) {
+            TreeNode node = st2.pop();
+            ans.add(node.val);
+        }
     }
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
+        List<Integer> ans = new ArrayList<Integer>();
         traversal(root, ans);
         return ans;
     }
