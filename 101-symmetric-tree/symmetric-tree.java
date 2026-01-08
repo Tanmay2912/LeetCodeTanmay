c906658773d7006c15406641a77c15566f60f586
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-01-08 11:36:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (33.41%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -14,11 +14,15 @@
  * }
  */
 // for this i actually hypothetically need to divide the tree into two halves
+// and then do post order traversal to find the symmetry inxthe bt
 class Solution {
     public boolean check(TreeNode left, TreeNode right) {
-        if(left == null && right == null) return true;
-        if(left == null || right == null) return false;
-        if(left.val != right.val) return false;
+        if(left == null && right == null) 
+            return true;
+        if(left == null || right == null) 
+            return false;
+        if(left.val != right.val) 
+            return false;
         return check(left.left, right.right) &&
                check(left.right, right.left);
     }
