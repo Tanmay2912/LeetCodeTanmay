536afe55e9ea1ded4acc9065b15947bb4caccfea
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-01-14 22:11:56 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (28.33%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -18,6 +18,25 @@ class Solution {
         return height(root) != -1;
     }
     public int height(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+        if(lh == -1 || rh == -1) {
+            return -1;
+        }
+        if(Math.abs(lh-rh) > 1) {
+            return -1;
+        }
+        return 1 + Math.max(lh, rh);
+    }
+}
+/*class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+    public int height(TreeNode root) {
         if(root == null) 
             return 0;
         int lh = height(root.left);
@@ -26,6 +45,6 @@ class Solution {
             return -1;
         if(Math.abs(lh-rh) > 1)
             return -1;
-        return 1+Math.max(lh, rh);
+        return 1 + Math.max(lh, rh);
     }
-}
+}*/
