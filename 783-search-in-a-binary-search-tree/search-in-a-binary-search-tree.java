d6aada870a3e1e966fc29fd4d4855791bbb2eb64
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-02-16 19:10:07 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (89.04%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -29,19 +29,6 @@ class Solution {
             return searchBST(root.right, val);
             //root = root.right;
         }
+        //return null;
     }
 }
-/*class Solution {
-    public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null) {
-            return null;
-        }
-        if (root.val == val) {
-            return root;
-        } else if (root.val > val) {
-            return searchBST(root.left, val);
-        } else {
-            return searchBST(root.right, val);
-        }        
-    }
-}*/
