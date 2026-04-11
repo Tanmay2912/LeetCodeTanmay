e67159ee6e4bd9287be898a290dcc6efa6a18eb4
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-04-11 17:09:07 +0530

    Time: 6 ms (6.99%) | Memory: 49.1 MB (17.88%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int findCircleNum(int[][] isConnected) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
         for(int i = 0; i < isConnected.length; i++) {
@@ -29,5 +29,47 @@ class Solution {
                 dfs(adj, visited, it);
             }
         }
+    }
+}*/
+
+class Solution {
+    public void bfsTraversal(ArrayList<ArrayList<Integer>> adj, Queue<Integer> q, boolean visited[], int node) {
+        q.offer(node);
+        visited[node] = true;
+        while(!q.isEmpty()) {
+            int vertex = q.peek();
+            q.remove();
+            for(int it : adj.get(vertex)) {
+                if(visited[it] == false) {
+                    q.offer(it);
+                    visited[it] = true;
+                }
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean visited[] = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        for(int i = 0; i < isConnected.length; i++) {
+            adj.add(new ArrayList<Integer>());
+        }
+        for(int i = 0; i < isConnected.length; i++) {
+            for(int j = 0; j < isConnected[0].length; j++) {
+                if(isConnected[i][j] == 1 && j!=i) {
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
+            }
+        }
+        int count = 0;
+        for(int i = 0; i < isConnected.length; i++) {
+            if(visited[i] == false) {
+                count += 1;
+                bfsTraversal(adj, q, visited, i);
+            }
+        }
+        return count;
     }
 }
