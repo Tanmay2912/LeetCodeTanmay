cab68f18ca8a97d8f129a4f88b42af3caab569d1
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-06-01 13:52:32 +0530

    Time: 6 ms (41.78%) | Memory: 44.4 MB (69.64%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -1,27 +1,13 @@
-// This fails because here for the length 2 array first element is indexed 0 and 0%3==0 hence that element is not added  in the total amount for that we need keep a count indexed 1
-/*class Solution {
-    public int minimumCost(int[] cost) {
-        Arrays.sort(cost);
-        int Mcost = 0;
-        for(int i = cost.length-1; i >= 0; i--) {
-            if(i%3 == 0) {
-                continue;
-            }
-            Mcost += cost[i];
-        }
-        return Mcost;
-    }
-}*/
 class Solution {
     public int minimumCost(int[] cost) {
         Arrays.sort(cost);
-        int Mcost = 0;
-        for(int i = cost.length-1, count = 1; i >= 0; i--, count++) {
-            if(count%3 == 0) {
+        int minCost = 0;
+        for(int i = cost.length-1, count = 1; i >=0; i--, count++) {
+            if(count % 3 == 0) {
                 continue;
             }
-            Mcost += cost[i];
+            minCost += cost[i];
         }
-        return Mcost;
+        return minCost;
     }
 }
