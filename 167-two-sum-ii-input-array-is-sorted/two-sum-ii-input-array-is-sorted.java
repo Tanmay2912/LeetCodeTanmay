8e6c093eb1917041c54c2e3ecc8f2c120969a5cb
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-06-04 09:39:59 +0530

    Time: 2 ms (96.37%) | Memory: 48.2 MB (89.53%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,18 +1,37 @@
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         int low = 0, high = numbers.length - 1;
+//         while (low < high) {
+//             int sum = numbers[low] + numbers[high];
+//             if (sum == target) {
+//                 return new int[] {low + 1, high + 1}; 
+//             } 
+//             else if (sum < target) {
+//                 low++;
+//             } 
+//             else {
+//                 high--;
+//             }
+//         }
+//         return new int[] {}; 
+//     }
+// }
+// Done using two pointer approach
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int low = 0, high = numbers.length - 1;
-        while (low < high) {
-            int sum = numbers[low] + numbers[high];
-            if (sum == target) {
-                return new int[] {low + 1, high + 1}; 
-            } 
-            else if (sum < target) {
-                low++;
-            } 
+        int left = 0, right = numbers.length-1;
+        while(left < right) {
+            int sum = numbers[left] + numbers[right];
+            if(sum == target) {
+                return new int[] {left+1, right+1};
+            }
+            else if(sum > target) {
+                right--;
+            }
             else {
-                high--;
+                left++;
             }
         }
-        return new int[] {}; 
+        return new int[] {}; // returning empty array
     }
 }
