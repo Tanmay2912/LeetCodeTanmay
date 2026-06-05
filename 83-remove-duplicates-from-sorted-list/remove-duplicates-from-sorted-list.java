7c3f138e5a036e16a444a0f131b8db47f9859070
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-06-05 09:42:34 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (83.08%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -8,18 +8,37 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+//         ListNode curr = head.next;
+//         ListNode prev = head;
+        
+//         while (curr != null) {
+//             if (curr.val == prev.val) {
+//                 prev.next = curr.next;
+//             } else {
+//                 prev = curr;
+//             }
+//             curr = curr.next;
+//         }
+//         return head;
+//     }
+// }
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null) {
+        if(head == null || head.next == null) {
             return head;
         }
         ListNode curr = head.next;
         ListNode prev = head;
-        
-        while (curr != null) {
-            if (curr.val == prev.val) {
+        while(curr != null) {
+            if(curr.val == prev.val) {
                 prev.next = curr.next;
-            } else {
+            }
+            else {
                 prev = curr;
             }
             curr = curr.next;
@@ -27,3 +46,24 @@ class Solution {
         return head;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
