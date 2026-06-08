22221bab7fb700d899b5b88ea32fc38436ac7fbc
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-06-08 10:26:31 +0530

    Time: 17 ms (8.85%) | Memory: 161 MB (10.32%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,25 +1,60 @@
+// class Solution {
+//     public int[] pivotArray(int[] nums, int pivot) {
+//         ArrayList<Integer> smaller = new ArrayList<>();
+//         ArrayList<Integer> equal = new ArrayList<>();
+//         ArrayList<Integer> greater = new ArrayList<>();
+//         for(int num : nums) {
+//             if(num < pivot) {
+//                 smaller.add(num);
+//             } 
+//             else if(num == pivot) {
+//                 equal.add(num);
+//             } 
+//             else {
+//                 greater.add(num);
+//             }
+//         }
+
+//         int[] result = new int[nums.length];
+//         int idx = 0;
+//         for(int x : smaller) result[idx++] = x;
+//         for(int x : equal) result[idx++] = x;
+//         for(int x : greater) result[idx++] = x;
+//         return result;
+//     }
+// }
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        ArrayList<Integer> smaller = new ArrayList<>();
-        ArrayList<Integer> equal = new ArrayList<>();
-        ArrayList<Integer> greater = new ArrayList<>();
-        for(int num : nums) {
-            if(num < pivot) {
+
+        List<Integer> smaller = new ArrayList<>();
+        List<Integer> equal = new ArrayList<>();
+        List<Integer> greater = new ArrayList<>();
+
+        for (int num : nums) {
+            if (num < pivot) {
                 smaller.add(num);
-            } 
-            else if(num == pivot) {
+            } else if (num == pivot) {
                 equal.add(num);
-            } 
-            else {
+            } else {
                 greater.add(num);
             }
         }
 
         int[] result = new int[nums.length];
-        int idx = 0;
-        for(int x : smaller) result[idx++] = x;
-        for(int x : equal) result[idx++] = x;
-        for(int x : greater) result[idx++] = x;
+        int index = 0;
+
+        for (int num : smaller) {
+            result[index++] = num;
+        }
+
+        for (int num : equal) {
+            result[index++] = num;
+        }
+
+        for (int num : greater) {
+            result[index++] = num;
+        }
+
         return result;
     }
 }
