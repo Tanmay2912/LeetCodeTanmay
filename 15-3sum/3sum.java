78bdd22c5a0ee80036f5e6925af49a7dde611aac
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-06-10 16:11:20 +0530

    Time: 35 ms (43.04%) | Memory: 59.2 MB (44.90%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,36 +1,5 @@
 /*class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();  
-        int n = nums.length;    
-        Arrays.sort(nums);
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int j = i + 1;
-            int k = n - 1;         
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];             
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[j]);
-                    temp.add(nums[k]);
-                    ans.add(temp);
-                    j++;
-                    k--;
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    while (j < k && nums[k] == nums[k + 1]) k--;
-                }
-            }
-        }
-        return ans;
-    }
-}*/
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
         for(int i = 0; i < nums.length-2; i++) {
@@ -56,6 +25,30 @@ class Solution {
                 else {
                     right--;
                 }
+            }
+        }
+        return ans;
+    }
+}*/
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length-2; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int left = i+1;
+            int right = nums.length-1;
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while(left < right && nums[left] == nums[left-1]) left++;
+                    while(left < right && nums[right] == nums[right+1]) right--;
+                }
+                else if(sum < 0) left++;
+                else right--;
             }
         }
         return ans;
