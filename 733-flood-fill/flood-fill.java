10b2510401a0eb24ab69878d7a387fbe2fead8ee
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-06-15 23:07:58 +0530

    Time: 1 ms (38.05%) | Memory: 46.9 MB (42.02%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public static boolean isValid(int i, int j, int n, int m) {
         if(i < 0 || i >= n) return false;
         if(j < 0 || j >= m) return false;
@@ -32,6 +32,36 @@ class Solution {
         // used dfs traversal but we can use bfs also
         dfs(sr, sc, ans, image, color, inic);
         return ans;
+    } 
+}*/
+class Solution {
+    public static boolean isValid(int i, int j, int n, int m) {
+        if(i < 0 || i >= n) return false;
+        if(j < 0 || j >= m) return false;
+        return true;
     }
-    
+    public static void dfs(int sr, int sc, int[][] ans, int[][] image, int color, int inic) {
+        ans[sr][sc] = color;
+        int n = image.length;
+        int m = image[0].length;
+        int delRow[] = {-1, 0, +1, 0};
+        int delCol[] = {0, 1, 0, -1};
+        for(int i = 0; i < 4; i++) {
+            int nrow = sr + delRow[i];
+            int ncol = sc + delCol[i];
+            if(isValid(nrow, ncol, n, m) && ans[nrow][ncol] == inic && ans[nrow][ncol] != color) {
+                dfs(nrow, ncol, ans, image, color, inic);
+            }
+        }
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int inic = image[sr][sc];
+        if(inic == color) return image;
+        int ans[][] = new int[image.length][image[0].length];
+        for(int i = 0; i < image.length; i++) {
+            ans[i] = Arrays.copyOf(image[i], image[i].length);
+        }
+        dfs(sr, sc, ans, image, color, inic);
+        return ans;
+    }
 }
