7d566ffbf00d79fd1b4fc9c033cf81fe937e855d
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-07-03 19:24:46 +0530

    Time: 1 ms (99.96%) | Memory: 77.5 MB (19.22%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,12 +1,15 @@
 // this is kadane's algorithm
 class Solution {
     public int maxSubArray(int[] nums) {
+        // har index p best ending nikal lo
         int bestending = nums[0];
         int maxSum = nums[0];
         for(int i = 1; i < nums.length; i++) {
             int c1 = bestending + nums[i];
             int c2 = nums[i];
+            // pahle ek optimal best ending nikalo
             bestending = Math.max(c1, c2);
+            // fir uss bestending ko ans k liye consider karo
             maxSum = Math.max(maxSum, bestending);
         }
         return maxSum;
