7a46b944fcfb07b0c5a010ebd50885f3709fe16a
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-07-22 09:57:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (14.01%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -8,6 +8,20 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/*class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null;
+        ListNode front = null;
+        while(temp != null) {
+            front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+}*/
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode temp = head;
@@ -22,5 +36,3 @@ class Solution {
         return prev;
     }
 }
-
- 
