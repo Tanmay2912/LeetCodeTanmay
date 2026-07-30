d1a993f4955bd294abe98447730f778e340e0325
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-07-30 15:34:54 +0530

    Time: 6 ms (73.01%) | Memory: 45.1 MB (68.23%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,24 +1,35 @@
 class Solution {
-    public void generateCombinations(int idx, int target, int arr[], List<List<Integer>> ans, List<Integer> ds) {
-        if(idx == arr.length) {
+    public void generateCombinations(int[] candidates, int target, List<List<Integer>> ans, int idx, List<Integer> ds) {
+        if(idx == candidates.length) {
             if(target == 0) {
                 ans.add(new ArrayList<>(ds));
             }
             return;
         }
-        if(arr[idx] <= target) {
-            ds.add(arr[idx]);
-            generateCombinations(idx+1, target-arr[idx], arr, ans, ds);
+        //choose
+        if(candidates[idx] <= target) {
+            ds.add(candidates[idx]);
+            generateCombinations(candidates, target-candidates[idx], ans, idx+1, ds);
             ds.remove(ds.size()-1);
         }
-        int next = idx + 1;
-        while (next < arr.length && arr[next] == arr[idx]) next++;
-        generateCombinations(next, target, arr, ans, ds);
+        int next = idx+1;
+        while(next < candidates.length && candidates[next] == candidates[idx]) next++;
+        generateCombinations(candidates, target, ans, next, ds);
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        // this is done to escape the duplicates eaisly
         Arrays.sort(candidates);
-        generateCombinations(0, target, candidates, ans, new ArrayList<Integer>());
+        int idx = 0;
+        generateCombinations(candidates, target, ans, idx, ds);
         return ans;
     }
 }
+
+
+
+
+
+
+
