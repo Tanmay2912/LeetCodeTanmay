bebd7d48410768035d3503de1b9faed5746ba032
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-07-30 14:34:24 +0530

    Time: 1 ms (59.37%) | Memory: 49.1 MB (9.32%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,61 +1,56 @@
 // class Solution {
-//     public int[] prefixMax(int[] height) {
-//         int[] prefix = new int[height.length];
-//         prefix[0] = height[0];
-//         for(int i = 1; i < height.length-1; i++) {
-//             prefix[i] = Math.max(prefix[i-1], height[i]);
-//         }
-//         for(int i = 0; i < prefix.height; i++) {
-//             System.out.print(prefix[i] + " ");
-//         }
-//         return prefix;
-//     }
-//     public int[] suffixMax(int[] height) {
-//         int suffix[] = new int[height.length];
-//         suffix[height.length-1] = height[height.length-1];
-//         for(int i = height.length-2; i >= 0; i--) {
-//             suffix[i] = Math.max(suffix[i+1], height[i]);
-//         }
-//         return suffix;
-//     }
 //     public int trap(int[] height) {
-//         int prefix[] = prefixMax(height);
-//         int suffix[] = suffixMax(height);
+//         int left = 0, right = height.length-1;
+//         int lMax = 0, rMax = 0;
 //         int total = 0;
-//         for(int i = 0; i < height.length; i++) {
-//             if(height[i] < prefix[i] && height[i] < suffix[i]) {
-//                 total += (Math.min(prefix[i], suffix[i]) - height[i]);
+//         while(left < right) {
+//             if(height[left] < height[right]) {
+//                 if(height[left] > lMax) {
+//                     lMax = height[left];
+//                 }
+//                 else {
+//                     total += lMax - height[left];
+//                 } 
+//                 left++;
+//             }
+//             else {
+//                 if(height[right] >= rMax) {
+//                     rMax = height[right];
+//                 }
+//                 else {
+//                     total += rMax - height[right];
+//                 }
+//                 right--;
 //             }
 //         }
 //         return total;
 //     }
 // }
-
+// brute force approach 
+// here we will use prefixMax and suffixMax of the array
+// use formula (min(leftMax, rightMax)-height[i])
 class Solution {
-    public int trap(int[] height) {
-        int left = 0, right = height.length-1;
-        int lMax = 0, rMax = 0;
-        int total = 0;
-        while(left < right) {
-            if(height[left] < height[right]) {
-                if(height[left] > lMax) {
-                    lMax = height[left];
-                }
-                else {
-                    total += lMax - height[left];
-                } 
-                left++;
-            }
-            else {
-                if(height[right] >= rMax) {
-                    rMax = height[right];
-                }
-                else {
-                    total += rMax - height[right];
-                }
-                right--;
-            }
+    public void prefixMax(int[] height, int[] preMax) {
+        preMax[0] = height[0];
+        for(int i = 1; i < height.length; i++) {
+            preMax[i] = Math.max(preMax[i-1], height[i]);
         }
-        return total;
+    } 
+    public void suffixMax(int[] height, int[] sufMax) {
+        sufMax[height.length-1] = height[height.length-1];
+        for(int i = height.length-2; i >= 0; i--) {
+            sufMax[i] = Math.max(sufMax[i+1], height[i]);
+        }
+    }
+    public int trap(int[] height) {
+        int ans = 0;
+        int preMax[] = new int[height.length];
+        int sufMax[] = new int[height.length];
+        prefixMax(height, preMax);
+        suffixMax(height, sufMax);
+        for(int i = 0; i < height.length; i++) {
+            ans += (Math.min(preMax[i], sufMax[i]))-height[i];
+        }
+        return ans;
     }
 }
