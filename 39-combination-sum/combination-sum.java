1febccc7de4c526a2f9bf0c9cf271a2befecabd9
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-07-30 15:14:26 +0530

    Time: 2 ms (96.39%) | Memory: 46 MB (18.62%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,33 @@
 class Solution {
-    public void generateCombinations(int idx, int target, int arr[], List<List<Integer>> ans, List<Integer> ds) {
-        if(idx == arr.length) {
+    public void generatePairs(int[] candidates, int target, List<List<Integer>> ans, int idx, List<Integer> ds) {
+        if(idx == candidates.length) {
             if(target == 0) {
                 ans.add(new ArrayList<>(ds));
             }
             return;
         }
-        if(arr[idx] <= target) {
-            ds.add(arr[idx]);
-            generateCombinations(idx, target-arr[idx], arr, ans, ds);
+        // choosen
+        if(candidates[idx] <= target) {
+            ds.add(candidates[idx]);
+            generatePairs(candidates, target-candidates[idx], ans, idx, ds);
             ds.remove(ds.size()-1);
         }
-        generateCombinations(idx+1, target, arr, ans, ds);
+        //not choosen
+        generatePairs(candidates, target, ans, idx+1, ds);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        generateCombinations(0, target, candidates, ans, new ArrayList<>());
+        int idx = 0;
+        List<Integer> ds = new ArrayList<>();
+        generatePairs(candidates, target, ans, idx, ds);
         return ans;
     }
 }
+
+
+
+
+
+
+
+
