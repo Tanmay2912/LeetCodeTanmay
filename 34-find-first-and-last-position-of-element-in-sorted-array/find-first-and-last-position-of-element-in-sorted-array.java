01502f10f7194b0e2742b9912db7ae451b21d684
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-08-04 15:21:28 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (52.32%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,32 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int low = 0, high = nums.length-1;
         int ans[] = new int[2];
         Arrays.fill(ans, -1);
-        int low = 0, high = nums.length - 1;
-
-        // To find first occurrence of the element i.e. lower bound 
+        // to find first position we will use lower bound
         while(low <= high) {
-            int mid = (low + high) / 2;
+            int mid = (low+high)/2;
             if(nums[mid] >= target) {
-                if(nums[mid] == target) {
-                    ans[0] = mid;
-                }
-                high = mid - 1;
+                if(nums[mid] == target) ans[0] = mid;
+                high = mid-1;
             }
             else {
-                low = mid + 1;
+                low = mid+1;
             }
         }
-
-        // To find last occurrence of the element i.e. upper bound
-        low = 0;
-        high = nums.length - 1;
+        low = 0; 
+        high = nums.length-1;
+        // to find last occurence we will use upper bound
         while(low <= high) {
-            int mid = (low + high) / 2;
+            int mid = (low+high)/2;
             if(nums[mid] > target) {
-                high = mid - 1;
+                high = mid-1;
             }
             else {
                 if(nums[mid] == target) {
                     ans[1] = mid;
                 }
-                low = mid + 1;
+                low = mid+1;
             }
         }
         return ans;
