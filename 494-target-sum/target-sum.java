3483c7d81743112540d6846f41cd8023c80acf01
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-08-07 22:51:46 +0530

    Time: 4 ms (87.92%) | Memory: 45.2 MB (39.35%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,7 +1,5 @@
 class Solution {
-
     static int subset(int[] arr, int target, int n, int[][] dp) {
-
         if (n == 0) {
             if (target == 0 && arr[0] == 0)
                 return 2;
@@ -9,40 +7,28 @@ class Solution {
                 return 1;
             return 0;
         }
-
         if (dp[n][target] != -1)
             return dp[n][target];
-
         int notTake = subset(arr, target, n - 1, dp);
-
         int take = 0;
         if (arr[n] <= target) {
             take = subset(arr, target - arr[n], n - 1, dp);
         }
-
         return dp[n][target] = take + notTake;
     }
-
     public int findTargetSumWays(int[] nums, int target) {
-
         int sum = 0;
         for (int x : nums)
             sum += x;
-
         // Impossible cases
         if (Math.abs(target) > sum)
             return 0;
-
         if ((sum + target) % 2 != 0)
             return 0;
-
         int requiredSum = (sum + target) / 2;
-
         int[][] dp = new int[nums.length][requiredSum + 1];
-
         for (int[] row : dp)
             Arrays.fill(row, -1);
-
         return subset(nums, requiredSum, nums.length - 1, dp);
     }
 }
