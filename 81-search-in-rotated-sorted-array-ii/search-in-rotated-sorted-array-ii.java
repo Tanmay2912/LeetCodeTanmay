0886b819c9a85082f932833c24fe4d0b9312695d
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-08-10 13:57:49 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (66.61%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,36 +1,29 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
-        
+        int low = 0, high = nums.length-1;
         while(low <= high) {
-            int mid = (low + high) / 2;
-
-            while (low < mid && nums[low] == nums[mid]) low++;
-            while (high > mid && nums[high] == nums[mid]) high--;
-            /*
-            if(nums[low] == nums[high] && nums[high] == nums[mid]) {
-                low += 1; 
-                high -= 1;
-                continue;
-            }*/
-
+            int mid = (low + high)/2;
             if(nums[mid] == target) {
                 return true;
             }
-
-            // left half being sorted
+            // to skip duplicates
+            while(low < mid && nums[low] == nums[mid]) {
+                low++;
+            }
+            while(high > mid && nums[high] == nums[mid]) {
+                high--;
+            }
+            // check if left half itself is sorted
             if(nums[low] <= nums[mid]) {
                 if(nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;       
+                    high = mid - 1;
                 }
                 else {
                     low = mid + 1;
                 }
             }
-
-            // right half being sorted
             else {
-                if(nums[mid] < target && target <= nums[high]) {
+                if(target <= nums[high] && nums[mid] <= target) {
                     low = mid + 1;
                 }
                 else {
