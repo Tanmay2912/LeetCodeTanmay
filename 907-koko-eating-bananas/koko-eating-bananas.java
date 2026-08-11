24a5490f92f3ec0dde7fbd818d38e804b74b94ac
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-08-11 12:06:22 +0530

    Time: 8 ms (67.08%) | Memory: 48.3 MB (21.94%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,30 +1,29 @@
 class Solution {
-    public int findSumOfPile(int[] piles, int mid, int h) {
-        int sum = 0;
-        for (int pile : piles) {
-            sum += (pile + mid - 1) / mid; 
-            if (sum > h) return sum; 
+    public long findSum(int piles[], int k) {
+        long sum = 0;
+        for (int i = 0; i < piles.length; i++) {
+            sum += (piles[i] + (long)(k - 1)) / k;
         }
         return sum;
     }
-
     public int minEatingSpeed(int[] piles, int h) {
         int low = 1;
-        int high = Arrays.stream(piles).max().getAsInt();
-        int ans = high; 
-
+        int high = 0;
+        for (int pile : piles) {
+            high = Math.max(high, pile);
+        }
+        int ans = high;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            int sum = findSumOfPile(piles, mid, h);
-
-            if (sum <= h) { 
+            long hours = findSum(piles, mid);
+            if (hours <= h) {
                 ans = mid;
                 high = mid - 1;
-            } else {
-                low = mid + 1; 
+            } 
+            else {
+                low = mid + 1;
             }
         }
-
         return ans;
     }
 }
