f3c0d33f7a2f7f6a482ba56adbbe696add18bf25
Tanmay Singh <149605165+Tanmay2912@users.noreply.github.com>
2026-09-21 11:11:06 +0530

    Time: 955 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,5 +1,8 @@
 /* Write your T-SQL query statement below */
-SELECT p.firstName, p.lastName, a.city, a.state 
-FROM Person as p
+-- I want all data of table person and city and state from table address hence person becomes left table and address becomes right table and if no match then null
+
+SELECT 
+    p.firstName, p.lastName, a.city, a.state
+FROM Person AS p
 LEFT JOIN Address as a
 ON p.personId = a.personId;
